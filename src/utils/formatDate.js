b9ff6d24366f07e25e306093320c2210aad4b37b
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2022-02-10 04:23:28 +0530

    date formatter utility code added

---
+++ src/utils/formatDate.js
@@ -0,0 +1,19 @@
+const formatDate = (date) => {
+  const d = new Date(date);
+  let month = `${d.getMonth() + 1}`;
+  let day = `${d.getDate()}`;
+  const year = d.getFullYear();
+
+  if (month.length < 2) {
+    //1-jan -> 01-jan
+    month = `0${month}`;
+  }
+
+  if (day.length < 2) {
+    day = `0${day}`;
+  }
+
+  return [year, month, day].join("-");
+};
+
+export default formatDate;
