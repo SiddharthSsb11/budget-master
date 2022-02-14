6b06913c667cce09bc93e6f01ada343837a5d867
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2022-02-14 05:57:45 +0530

    Alert minor bug fixed

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,4 +1,4 @@
-import React,{useEffect, useContext} from "react";
+import React, { useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import {
   Container,
@@ -12,55 +12,76 @@ import {
 } from "@chakra-ui/react";
 import Login from "../components/Authentication/Login";
 import Signup from "../components/Authentication/Signup";
-import {BudgetContext} from "../context/budget-context";
+import { BudgetContext } from "../context/budget-context";
+import Alerts from "../components/Alerts";
 
+//import { useToast } from "@chakra-ui/toast";
 
 const HomePage = () => {
   const { user } = useContext(BudgetContext);
   const navigate = useNavigate();
 
-/*   useEffect(() => {
-    if (user) navigate("/chats");
-  }, [navigate, user]) */;
+  /*   useEffect(() => {
+    if (user) {
+      navigate("/transactions")
+    }
+    
+    // eslint-disable-next-line
+  }, [navigate]);
+ */
 
   return (
-    <Container maxW="xl" centerContent>
-      <Box
-        d="flex"
-        justifyContent="center"
-        margin = "2rem 0 1rem 0"
-        p={3}
-        borderRadius="lg"
-        borderColor="black" borderWidth="1px"
-        w="100%"
-        bg="#0F3057"
-      >
-        <Text
-          color="gray.100"
-          fontSize="4xl"
-          fontFamily="Work sans"
-          fontWeight="bold"
-        >
-          Budget Master
-        </Text>
-      </Box>
-      <Box bg="purple.50" w="100%" p={4} borderRadius="lg" borderColor="black" borderWidth="1px">
-        <Tabs isFitted variant="soft-rounded" colorScheme="pink">
-          <TabList mb="1em">
-            <Tab fontWeight="bold" >Login</Tab>
-            <Tab fontWeight="bold">Sign Up</Tab>
-          </TabList>
-          <TabPanels>
-            <TabPanel>
-              <Login />
-            </TabPanel>
-            <TabPanel>
-              <Signup />
-            </TabPanel>
-          </TabPanels>
-        </Tabs>
-      </Box>
-    </Container>
+    <React.Fragment>
+      {user ? (
+        <Alerts />
+      ) : (
+        <Container maxW="xl" centerContent>
+          <Box
+            d="flex"
+            justifyContent="center"
+            margin="2rem 0 1rem 0"
+            p={3}
+            borderRadius="lg"
+            borderColor="black"
+            borderWidth="1px"
+            w="100%"
+            bg="#0F3057"
+          >
+            <Text
+              color="gray.100"
+              fontSize="4xl"
+              fontFamily="Work sans"
+              fontWeight="bold"
+            >
+              Budget Master
+            </Text>
+          </Box>
+          <Box
+            bg="purple.50"
+            w="100%"
+            p={4}
+            borderRadius="lg"
+            borderColor="black"
+            borderWidth="1px"
+          >
+            <Tabs isFitted variant="soft-rounded" colorScheme="pink">
+              <TabList mb="1em">
+                <Tab fontWeight="bold">Login</Tab>
+                <Tab fontWeight="bold">Sign Up</Tab>
+              </TabList>
+              <TabPanels>
+                <TabPanel>
+                  <Login />
+                </TabPanel>
+                <TabPanel>
+                  <Signup />
+                </TabPanel>
+              </TabPanels>
+            </Tabs>
+          </Box>
+        </Container>
+      )}
+    </React.Fragment>
   );
 };
 
--- src/Pages/TransactionPage.js
+++ src/Pages/TransactionPage.js
@@ -6,6 +6,7 @@ import Main from "../components/Main/Main";
 import { BudgetContext } from "../context/budget-context";
 import { useNavigate } from "react-router-dom";
 import { Button } from "@chakra-ui/button";
+import Alerts from "../components/Alerts";
 
 const TransactionPage = () => {
   const { user } = useContext(BudgetContext);
@@ -13,181 +14,91 @@ const TransactionPage = () => {
   const navigate = useNavigate();
 
   return (
-    <div style={{ width: "100%" }}>
-      <HeaderBar />
+    <React.Fragment>
       {!user ? (
-        <Box
-          d="flex"
-          flexDir="column"
-          justifyContent="space-between"
-          alignItems="center"
-          fontFamily="Work sans bold"
-          //width="50%"
-          marginTop="20px"
-          gap="23px"
-        >
-          <Button
-            width="30%"
-            //height="10vh"
-            fontWeight="bold"
-            colorScheme="yellow"
-            fontSize="3xl"
-            //style={{ marginTop: "15px" }}
-            onClick={() => navigate("/")}
-            p={8}
-          >
-            Start Saving
-          </Button>
-          <Button
-            width="30%"
-            //height="10vh"
-            fontWeight="bold"
-            colorScheme="purple"
-            fontSize="3xl"
-            //style={{ marginTop: "15px" }}
-            onClick={() => navigate("/")}
-            p={8}
-          >
-           And Managing
-          </Button>
-          <Button
-            width="30%"
-            //height="10vh"
-            fontWeight="bold"
-            colorScheme="yellow"
-            fontSize="3xl"
-            //style={{ marginTop: "15px" }}
-            onClick={() => navigate("/")}
-            p={8}
-          >
-            Your Budget
-          </Button>
-          <Button
-            width="30%"
-            //height="10vh"
-            fontWeight="bold"
-            colorScheme="purple"
-            fontSize="3xl"
-            //style={{ marginTop: "15px" }}
-            onClick={() => navigate("/")}
-            p={8}
-          >
-            By
-          </Button>
-          <Button
-            width="30%"
-            //height="10vh"
-            fontWeight="bold"
-            colorScheme="yellow"
-            fontSize="3xl"
-            //style={{ marginTop: "15px" }}
-            onClick={() => navigate("/")}
-            p={8}
-          >
-            Clicking On
-          </Button>
-          <Button
-            width="30%"
-            //height="10vh"
-            fontWeight="bold"
-            colorScheme="purple"
-            fontSize="3xl"
-            //style={{ marginTop: "15px" }}
-            onClick={() => navigate("/")}
-            p={8}
-          >
-          Any of US
-          </Button>
-          <Button
-            width="30%"
-            //height="10vh"
-            fontWeight="bold"
-            colorScheme="yellow"
-            fontSize="3xl"
-            //style={{ marginTop: "15px" }}
-            onClick={() => navigate("/")}
-            p={8}
-          >
-            Please Login First !!
-          </Button>
-        
-        </Box>
+        <Alerts />
       ) : (
-        <Box
-          d="flex"
-          justifyContent="space-between"
-          alignItems="center"
-          width="100%"
-          p={6}
-          mt={3}
-          flexDir={{ base: "column", md: "row" }}
-          gap={{ base: "9", md: "3" }}
-          //h="75vh"
-          //bg="pink.400"
-        >
-          <Box
-            d={{ base: "none", md: "flex" }}
-            bg="green.50"
-            alignItems="center"
-            justifyContent="center"
-            w={{ base: "85%", md: "31%" }}
-            border="1.5px solid black"
-            borderBottom="0.8em solid #00ff00;"
-            p={2}
-            borderRadius="md"
-
-            //flexGrow={1.5}
-          >
-            <Details title="Income" />
-          </Box>
+        <div style={{ width: "100%" }}>
+          <HeaderBar />
 
           <Box
             d="flex"
-            bg="purple.900"
-            //bg="#0F3057"
-            w={{ base: "85%", md: "33%" }}
+            justifyContent="space-between"
             alignItems="center"
-            justifyContent="center"
-            p={2}
-            borderRadius="md"
-            border="1.5px solid black"
-            alignSelf={{base:"center" , md:"flex-start"}}
-            borderBottom="0.8em solid #D53F8C"
+            width="100%"
+            p={6}
+            mt={3}
+            flexDir={{ base: "column", md: "row" }}
+            gap={{ base: "9", md: "3" }}
+            //h="75vh"
+            //bg="pink.400"
           >
-            <Main />
-          </Box>
+            <Box
+              d={{ base: "none", md: "flex" }}
+              bg="green.50"
+              alignItems="center"
+              justifyContent="center"
+              w={{ base: "85%", md: "31%" }}
+              border="1.5px solid black"
+              borderBottom="0.8em solid #00ff00;"
+              p={2}
+              borderRadius="md"
 
-          <Box
-            d={{ base: "flex", md: "none" }}
-            bg="green.50"
-            border="1.5px solid black"
-            borderBottom="0.8em solid #00ff00;"
-            p={2}
-            w={{ base: "85%", md: "0%" }}
-            alignItems="center"
-            justifyContent="center"
-            borderRadius="md"
-          >
-            <Details title="Income" />
-          </Box>
+              //flexGrow={1.5}
+            >
+              <Details title="Income" />
+            </Box>
 
-          <Box
-            p={2}
-            d="flex"
-            bg="red.50"
-            w={{ base: "85%", md: "31%" }}
-            border="1.5px solid black"
-            borderBottom="0.8em solid #F54748"
-            // flexGrow={1.5}
-            alignItems="center"
-            justifyContent="center"
-            borderRadius="md"
-          >
-            <Details title="Expense" />
+            <Box
+              d="flex"
+              bg="purple.900"
+              //bg="#0F3057"
+              w={{ base: "85%", md: "33%" }}
+              alignItems="center"
+              justifyContent="center"
+              p={2}
+              borderRadius="md"
+              border="1.5px solid black"
+              alignSelf={{ base: "center", md: "flex-start" }}
+              //borderBottom="0.8em solid #D53F8C"
+              borderBottom="0.8em solid #805AD5
+            "
+            >
+              <Main />
+            </Box>
+
+            <Box
+              d={{ base: "flex", md: "none" }}
+              bg="green.50"
+              border="1.5px solid black"
+              borderBottom="0.8em solid #00ff00;"
+              p={2}
+              w={{ base: "85%", md: "0%" }}
+              alignItems="center"
+              justifyContent="center"
+              borderRadius="md"
+            >
+              <Details title="Income" />
+            </Box>
+
+            <Box
+              p={2}
+              d="flex"
+              bg="red.50"
+              w={{ base: "85%", md: "31%" }}
+              border="1.5px solid black"
+              borderBottom="0.8em solid #F54748"
+              // flexGrow={1.5}
+              alignItems="center"
+              justifyContent="center"
+              borderRadius="md"
+            >
+              <Details title="Expense" />
+            </Box>
           </Box>
-        </Box>
+        </div>
       )}
-    </div>
+    </React.Fragment>
   );
 };
 
