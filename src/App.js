a9c277d1b036532b2d5a73c5cba235f1a7bd24b5
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2022-03-02 00:54:33 +0530

    application re routing done

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/transactions" element={<TransactionPage />} />
-        <Route path="/:tId" element={<UpdateTransaction />} />
+        
       </Routes>
     </div>
   );
